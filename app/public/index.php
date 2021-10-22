33a636bd630816fd3bedef8225e10aa878c76876
WildBoni <web@davidebonifacio.it>
2021-10-22 18:15:21 +0200

    setting up mariadb

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,8 @@
 <?php
-phpinfo();
+$pdo = new PDO('mysql:dbname=testdb;host=mysql', 'testusr', 'testpw', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$query = $pdo->query('SHOW VARIABLES like "version"');
+
+$row = $query->fetch();
+
+echo 'MySQL version:' . $row['Value'];
